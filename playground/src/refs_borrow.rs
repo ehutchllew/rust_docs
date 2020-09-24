8494b501549dab69d0d280fb6d09e7826cf0499a
Evan H L <ehutch7@gmail.com>
2020-09-24 14:49:19 -0400

    adding some annotations to clarify what goes on.

--- playground/src/refs_borrow.rs
+++ playground/src/refs_borrow.rs
@@ -30,4 +30,9 @@ pub fn b() {
 fn b_calc(s: &String) -> usize {
     let length = s.len();
     return length;
+    // When this function returns `s` goes out of scope.
+    // if it owned whatever value was associated with `s` then
+    // that ref would be GC'd; however, since it's a ref type
+    // it simply REFERS to the value, but doesn't own it.
+    // This then has no side-effect when the function returns.
 }
