7085073b5ce150f77653556bd61ec900e6909114
Evan H L <ehutch7@gmail.com>
2020-09-26 08:01:04 -0400

    Adding some comments to clearing strings that shows why a slice type cannot have its original ref cleared before it leaves scope.

--- playground/src/slice_type.rs
+++ playground/src/slice_type.rs
@@ -2,6 +2,10 @@ pub fn a() {
     println!("\n*****\nSLICE_TYPE::A()\n*****\n");
     let s1 = String::from("slice type");
     let first_word: &str = a_word(&s1);
+
+    // s1.clear(); // This breaks because of first_word using s1 as an immutable reference, whereas
+    // this tries to change modify it with a mutable type. Which breaks rust's rules.
+
     println!("S1 is: {} \n first word is: {}", s1, first_word);
 }
 
